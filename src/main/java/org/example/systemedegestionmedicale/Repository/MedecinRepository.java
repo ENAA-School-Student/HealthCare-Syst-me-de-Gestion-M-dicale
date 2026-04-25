c261ed6fd3ed92cc669c6a9ab931b880f6855de6
zakariaoutla <zakariadevlop78@gmail.com>
2026-04-25 17:08:10 +0200

    création du repository PaMedecin

---
+++ src/main/java/org/example/systemedegestionmedicale/Repository/MedecinRepository.java
@@ -0,0 +1,7 @@
+package org.example.systemedegestionmedicale.Repository;
+
+import org.example.systemedegestionmedicale.Models.Medecin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedecinRepository extends JpaRepository<Medecin, Long> {
+}
