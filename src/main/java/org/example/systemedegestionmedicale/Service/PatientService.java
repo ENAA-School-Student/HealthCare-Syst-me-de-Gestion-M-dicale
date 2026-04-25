31bdeaad39063d6b56bac0f6f1b89349cd0115bf
zakariaoutla <zakariadevlop78@gmail.com>
2026-04-25 17:37:30 +0200

    ajout du service Patient avec la méthode ajouterPatient

---
+++ src/main/java/org/example/systemedegestionmedicale/Service/PatientService.java
@@ -0,0 +1,26 @@
+package org.example.systemedegestionmedicale.Service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.example.systemedegestionmedicale.Dto.PatientDto;
+import org.example.systemedegestionmedicale.Mapper.PatientMapper;
+import org.example.systemedegestionmedicale.Models.Patient;
+import org.example.systemedegestionmedicale.Repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PatientService {
+
+    private final PatientRepository patientRepository;
+    private final PatientMapper patientMapper;
+
+
+    public PatientDto save(PatientDto patientDto){
+
+        Patient entity = patientMapper.toEntity((patientDto));
+        Patient save = patientRepository.save(entity);
+        return patientMapper.toDto(save);
+
+    }
+}
