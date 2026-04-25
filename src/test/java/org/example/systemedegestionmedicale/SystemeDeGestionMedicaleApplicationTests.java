79669ef23090bc06b051ec88e7271788ff68698e
zakariaoutla <zakariadevlop78@gmail.com>
2026-04-25 14:15:46 +0200

    initialisation du projet et configuration des dépendances Maven

---
+++ src/test/java/org/example/systemedegestionmedicale/SystemeDeGestionMedicaleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.systemedegestionmedicale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemeDeGestionMedicaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
