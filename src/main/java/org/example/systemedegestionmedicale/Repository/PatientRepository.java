4e4334edb67dcf5aef2ad2712da1c1f98a81f137
zakariaoutla <zakariadevlop78@gmail.com>
2026-04-25 17:06:06 +0200

    création du repository Patient pour l'accès aux données

---
+++ src/main/java/org/example/systemedegestionmedicale/Repository/PatientRepository.java
@@ -0,0 +1,7 @@
+package org.example.systemedegestionmedicale.Repository;
+
+import org.example.systemedegestionmedicale.Models.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+}
