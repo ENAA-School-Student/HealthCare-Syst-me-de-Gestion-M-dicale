00c724027b896eb7bee41a2772b2ccb0b85d8da5
zakariaoutla <zakariadevlop78@gmail.com>
2026-04-25 14:57:09 +0200

    création de l'entité RendezVous avec gestion du statut enum

--- src/main/java/org/example/systemedegestionmedicale/Models/RendezVou.java
+++ src/main/java/org/example/systemedegestionmedicale/Models/RendezVou.java
@@ -1,4 +1,28 @@
 package org.example.systemedegestionmedicale.Models;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.example.systemedegestionmedicale.Enums.StatusRendezVou;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "rendezvous")
+@Data
 public class RendezVou {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private LocalDate dateRendezVous;
+    private StatusRendezVou status;
+
+
+    @ManyToOne
+    private Patient patient;
+
+    @ManyToOne
+    private Medecin medecin;
+
 }
