8ef70ff0dd93bb7f81b4a67bb5039f5d2e7109e3
zakariaoutla <zakariadevlop78@gmail.com>
2026-04-26 11:37:21 +0200

    Ajout de la méthode update dans le service et d’un endpoint PUT dans le controller. Test validé avec Swagger.

--- src/main/java/org/example/systemedegestionmedicale/Dto/DossierMedicalDto.java
+++ src/main/java/org/example/systemedegestionmedicale/Dto/DossierMedicalDto.java
@@ -7,8 +7,6 @@ import java.time.LocalDate;
 
 @Data
 public class DossierMedicalDto {
-
-    private long id;
     private String diagnostic;
     private String observation;
     private LocalDate dateCreation;
