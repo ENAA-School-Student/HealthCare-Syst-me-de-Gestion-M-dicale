d86ba880154078fafabcb0665dd0bf07bda09bfb
zakaria outla <zakariadevlop78@gmail.com>
2026-06-12 15:45:05 +0100

    done

--- src/main/java/org/example/systemedegestionmedicale/configuration/JwtUtil.java
+++ src/main/java/org/example/systemedegestionmedicale/configuration/JwtUtil.java
@@ -14,7 +14,7 @@ import java.util.Date;
 
 @Component
 public class JwtUtil {
-    @Value("${APP_JWT_SECRET}")
+    @Value("${app.jwt.secret}")
     private String secret;
     @Value("${app.jwt.expiration}")
     private long expiration;
